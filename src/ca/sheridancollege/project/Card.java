70e5380c50d5aa2cec2ca63a14a9e2b3e0adbd36
SainiRajat1407 <105829997+SainiRajat1407@users.noreply.github.com>
2022-08-17 15:47:15 -0400

    Update Cards Class

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -15,12 +15,81 @@ public class Card {
     //default modifier for child classes
     private String cardName;
     private int cardRank; // rank value of card
+    private String suit;
+    
+    final static String[][] cardDetails = {{"KING","13"},{"QUEEN","12"},{"JACK","11"},{"TEN","10"},{"NINE","9"},
+                       {"EIGHT","8"},{"SEVEN","7"},{"SIX","6"},{"FIVE","5"},{"FOUR","4"},
+                       {"THREE","3"},{"TWO","2"},{"ACE","14"}};
+    final static String[] suitName = {"DIAMONDS","SPADE", "HEARTS","CLUBS"};
+    /**
+     * no-args Constructor
+     */
+    public Card (){}
+    
+    /**
+     * constructor
+     * @param cardName
+     * @param cardRank 
+     * @param suit
+     */
+    public Card (String suit,String cardName,int cardRank){
+        this.cardName = cardName;
+        this.cardRank = cardRank;
+        this.suit = suit;
+    }
+    
+    /**
+     * @return the cardName
+     */
+    public String getCardName() {
+        return cardName;
+    }
+
+    /**
+     * @param cardName the cardName to set
+     */
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    /**
+     * @return the cardRank
+     */
+    public int getCardRank() {
+        return cardRank;
+    }
+
+    /**
+     * @param cardRank the cardRank to set
+     */
+    public void setCardRank(int cardRank) {
+        this.cardRank = cardRank;
+    }
+    /**
+     * @return the suit
+     */
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * @param suit the suit to set
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
     
     /**
      * Students should implement this method for their specific children classes
      *
      * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
      */
+    
+    public String toString(){
+        String printCard = "[SUIT] "+suit+" [CARD NAME] "+cardName + " [CARD RANK] "+ cardRank;
+        return printCard;
+    }
 
-
+    
 }
